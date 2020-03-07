6b52c71ca58e6930e8a3578b5c4a97829e6a77e5
Mateus Gonçalves Ferreira <mateusgf@hotmail.com>
2020-03-07 18:42:04 -0300

    feat: Implements other way to CreateMap

---
+++ src/TopshelfCleanArchitecture.Domain/Entities/Configuration/AssemblyTypes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopshelfCleanArchitecture.Domain.Entities.Configuration
+{
+    public class AssemblyTypes
+    {
+        public List<Type> Model { get; set; }
+    }
+}
--- src/TopshelfCleanArchitecture.Infra.CrossCutting/AutoMapper/InfraProfile.cs
+++ src/TopshelfCleanArchitecture.Infra.CrossCutting/AutoMapper/InfraProfile.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using TopshelfCleanArchitecture.Domain.Entities.Base;
+using TopshelfCleanArchitecture.Domain.Entities.Configuration;
 using TopshelfCleanArchitecture.Infra.Data.NHibernateDataAccess.DataModels.Base;
 
 namespace TopshelfCleanArchitecture.Infra.CrossCutting.AutoMapper
@@ -11,24 +13,50 @@ namespace TopshelfCleanArchitecture.Infra.CrossCutting.AutoMapper
     {
         public InfraProfile()
         {
+            IDictionary<string, List<Type>> dictionaryTypes = new Dictionary<string, List<Type>>();
+
             var types = AppDomain.CurrentDomain.GetAssemblies()
                .SelectMany(s => s.GetTypes())
                .Where(p => (typeof(DataModel).IsAssignableFrom(p) || typeof(DomainModel).IsAssignableFrom(p)) && p.IsPublic && !p.IsAbstract)
                .Distinct()
                .ToList();
 
-            var dataModelList = types.Where(p => typeof(DataModel).IsAssignableFrom(p));
-            var domainModelList = types.Where(p => typeof(DomainModel).IsAssignableFrom(p));
-
-            foreach (var dataModel in dataModelList)
+            foreach (var type in types)
             {
-                var domainModel = domainModelList.FirstOrDefault(s => ReplaceLastOccurrence(s.Name, "Model", "") ==  ReplaceLastOccurrence(dataModel.Name, "Data", ""));
-                if (domainModel == null)
-                    continue;
-
-                CreateMap(domainModel, dataModel);
-                CreateMap(dataModel, domainModel);
+                string name = ReplaceLastOccurrence(ReplaceLastOccurrence(type.Name, "Model", ""), "Data", "");
+                if (dictionaryTypes.ContainsKey(name))
+                    dictionaryTypes[name].Add(type);
+                else
+                {
+                    dictionaryTypes.Add(name, new List<Type>());
+                    dictionaryTypes[name].Add(type);
+                }
             }
+
+            foreach (var dictionaryType in dictionaryTypes.Values)
+            {
+                CreateMap(dictionaryType[0], dictionaryType[1]);
+                CreateMap(dictionaryType[1], dictionaryType[0]);
+            }
+
+            //var types = AppDomain.CurrentDomain.GetAssemblies()
+            //   .SelectMany(s => s.GetTypes())
+            //   .Where(p => (typeof(DataModel).IsAssignableFrom(p) || typeof(DomainModel).IsAssignableFrom(p)) && p.IsPublic && !p.IsAbstract)
+            //   .Distinct()
+            //   .ToList();
+
+            //var dataModelList = types.Where(p => typeof(DataModel).IsAssignableFrom(p));
+            //var domainModelList = types.Where(p => typeof(DomainModel).IsAssignableFrom(p));
+
+            //foreach (var dataModel in dataModelList)
+            //{
+            //    var domainModel = domainModelList.FirstOrDefault(s => ReplaceLastOccurrence(s.Name, "Model", "") ==  ReplaceLastOccurrence(dataModel.Name, "Data", ""));
+            //    if (domainModel == null)
+            //        continue;
+
+            //    CreateMap(domainModel, dataModel);
+            //    CreateMap(dataModel, domainModel);
+            //}
         }
         string ReplaceLastOccurrence(string str, string toReplace, string replacement)
         {
